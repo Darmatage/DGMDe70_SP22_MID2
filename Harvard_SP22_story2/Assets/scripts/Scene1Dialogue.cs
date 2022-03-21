cbd413e246590cad9411b51b55cbfea5e371e1a2
thebearayu <98497344+thebearayu@users.noreply.github.com>
2022-03-21 12:31:57 -0400

    Update Scene 1

--- Harvard_SP22_story2/Assets/scripts/Scene1Dialogue.cs
+++ Harvard_SP22_story2/Assets/scripts/Scene1Dialogue.cs
@@ -14,11 +14,13 @@ public class Scene1Dialogue : MonoBehaviour {
         public Text Char3name;
         public Text Char3speech;
 		// public Text TextDelay;
-		public Text Char4speech_shake;
+		    public Text Char4speech_shake;
         public GameObject DialogueDisplay;
         public GameObject ArtChar1;
-        public GameObject ArtChar2;
         public GameObject ArtBG1;
+        public GameObject ArtBG2;
+        public GameObject ArtBG3;
+        public GameObject ArtBG4;
         public GameObject ChoiceEvade1;
         public GameObject ChoiceEvade2;
         public GameObject ChoiceFight;
@@ -32,8 +34,10 @@ public class Scene1Dialogue : MonoBehaviour {
 void Start(){         // initial visibility settings
         DialogueDisplay.SetActive(false);
         ArtChar1.SetActive(false);
-        ArtChar2.SetActive(false);
         ArtBG1.SetActive(true);
+        ArtBG2.SetActive(false);
+        ArtBG3.SetActive(false);
+        ArtBG4.SetActive(false);
         ChoiceEvade1.SetActive(false);
         ChoiceEvade2.SetActive(false);
         ChoiceFight.SetActive(false);
@@ -57,7 +61,7 @@ public void talking(){         // main story function. Players hit next to progr
                 // AudioSource.Play();
         }
         else if (primeInt == 2){
-                ArtChar1.SetActive(true);
+                ArtChar1.SetActive(false);
                 DialogueDisplay.SetActive(true);
                 Char1name.text = "COMMAND";
                  StartCoroutine(TypeText(Char1speech, "This is Commander Zurcik hailing Captain Asana of the SI-627. Do you read me?"));
@@ -92,11 +96,12 @@ public void talking(){         // main story function. Players hit next to progr
         }
        else if (primeInt == 6){
                 Char1name.text = "COMMAND";
-                  StartCoroutine(TypeText(Char1speech,"Thank you, Captain. Anything to add, Lieutenant Rory"));
+                  StartCoroutine(TypeText(Char1speech,"Thank you, Captain. Anything to add, Lieutenant Rory "));
                 Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt == 7){
+                ArtChar1.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
@@ -105,6 +110,7 @@ public void talking(){         // main story function. Players hit next to progr
                 Char3speech.text = "No, sir. Systems are stable and functioning as expected. We picked up some chatter a few hours back, but I was unable to lock onto the signal. It’s probably nothing.";
 		}
        else if (primeInt == 8){
+                ArtChar1.SetActive(false);
                 Char1name.text = "COMMAND";
                   StartCoroutine(TypeText(Char1speech,"Thank you, Lieutenant. That may... {tchhkshhh} notice of a batt... {zztch} earby territory. Avoid at... {tzzkkshh} mission... {ktchhhhssshhhh}"));
                 Char2name.text = "";
@@ -131,6 +137,7 @@ public void talking(){         // main story function. Players hit next to progr
                // Choice1b.SetActive(true); // function Choice1bFunct()
         }
 		else if (primeInt == 10){
+                ArtChar1.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
@@ -142,7 +149,8 @@ public void talking(){         // main story function. Players hit next to progr
 
 
         else if (primeInt == 11){
-                ArtChar2.SetActive(true);
+                ArtBG1.SetActive(false);
+                ArtBG2.SetActive(true);
                 Char1name.text = "";
 				        Char1speech.text = "";
                 Char2name.text = "";
@@ -160,29 +168,39 @@ else if (primeInt == 12){
                 Char2speech.text = "";
                 Char3name.text = "";
                 Char3speech.text = "";
-                Char4speech_shake.text = "Shields up! 9";
+                Char4speech_shake.text = "Shields up!";
 
 			  }
 				else if (primeInt == 13){
+                ArtBG1.SetActive(false);
+                ArtBG2.SetActive(false);
+                ArtBG3.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
                 Char3name.text = "Lieutenant Rory";
 			          Char3speech.text = "";
-                Char4speech_shake.text = "Evasive maneuvers! 11";
+                Char4speech_shake.text = "Evasive maneuvers!";
         }
 
 				else if (primeInt == 14){
+                ArtBG1.SetActive(false);
+                ArtBG2.SetActive(false);
+                ArtBG3.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
-               Char3name.text = "";
+                Char3name.text = "";
                 Char4speech_shake.text = "With a hum, energy shields surround the ship. The next shot sends a ripple of energy across the porthole, but the ship holds stable.";
         }
 
 				else if (primeInt == 15){
+                ArtBG1.SetActive(false);
+                ArtBG2.SetActive(false);
+                ArtBG3.SetActive(false);
+                ArtBG4.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
@@ -306,7 +324,7 @@ else if (primeInt == 12){
         public void NextScene2Fun(){
                 SceneManager.LoadScene("Scene2");
         }
-		
+
 		    IEnumerator TypeText(Text target, string fullText){
                 float delay = 0.025f;
                 nextButton.SetActive(false);
