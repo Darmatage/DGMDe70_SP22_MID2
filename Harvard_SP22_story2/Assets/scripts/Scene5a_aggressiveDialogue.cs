03ecb51182cc9abcad22ad7a119159598f4eb95a
MineralFoliage <micahgreenleaf@gmail.com>
2022-03-28 14:26:05 -0400

    5a_aggressive laser and UI update

--- Harvard_SP22_story2/Assets/scripts/Scene5a_aggressiveDialogue.cs
+++ Harvard_SP22_story2/Assets/scripts/Scene5a_aggressiveDialogue.cs
@@ -30,7 +30,8 @@ public class Scene5a_aggressiveDialogue : MonoBehaviour {
     		public GameObject ArtLaser1;
     		public GameObject ArtLaser2;
     		public GameObject ArtLaser3;
-    		public GameObject ArtLaser4;
+            public GameObject ArtLaser4;
+            public GameObject ArtLaser5;
         public GameObject NextScene1Button;
 		    public GameObject TalkButton;
         public GameObject nextButton;
@@ -54,6 +55,7 @@ void Start(){         // initial visibility settings
     ArtLaser2.SetActive(false);
     ArtLaser3.SetActive(false);
     ArtLaser4.SetActive(false);
+    ArtLaser5.SetActive(false);
     NextScene1Button.SetActive(false);
     nextButton.SetActive(true);
     allowSpace = true;
@@ -64,7 +66,7 @@ void Start(){         // initial visibility settings
 
 		// input player name
 	  string pNameTemp = gameHandler.GetName();
-       playerName = pNameTemp.ToUpper();
+       playerName = pNameTemp;
     }
 
 void Update(){         // use spacebar as Next button
@@ -81,47 +83,53 @@ public void talking(){         // main story function. Players hit next to progr
 
 
 
-        if (primeInt == 1){
+        if (primeInt == 1)
+        {
+            ArtBG1.SetActive(true);
         }
-        else if (primeInt == 2){
-                DialogueDisplay.SetActive(true);
-                ArtBG3.SetActive(true);
-                Char1Plate.SetActive(false);
-                Char2Plate.SetActive(false);
-                Char3Plate.SetActive(true);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-                Char3name.text = "Lieutenant Rory";
-                Char3speech.text = "Airlocks attached, Captain. Doors open on your word.";
-				        Char4speech_shake.text = "";
-				        TalkButton.SetActive(true);
-				        ButtonText1.text = "Go!";
-				        nextButton.SetActive(false);
-                allowSpace = false;
+        else if (primeInt == 2)
+        {
+            DialogueDisplay.SetActive(true);
+            ArtBG3.SetActive(false);
+            ArtBG1.SetActive(true);
+            Char1Plate.SetActive(false);
+            Char2Plate.SetActive(false);
+            Char3Plate.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "Lieutenant Rory";
+            Char3speech.text = "Airlocks attached, Captain. Doors open on your word.";
+            Char4speech_shake.text = "";
+            TalkButton.SetActive(true);
+            ButtonText1.text = "Go!";
+            nextButton.SetActive(false);
+            allowSpace = true;
         }
-        else if (primeInt == 3){
-                ArtBG3.SetActive(false);
-                ArtBG1.SetActive(true);
-                ArtChar1.SetActive(true);
-                Char1Plate.SetActive(false);
-                Char2Plate.SetActive(true);
-                Char3Plate.SetActive(false);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = playerName;
-                Char2speech.text = "Make it so.";
-                Char3name.text = "";
-                Char3speech.text = "";
-				        Char4speech_shake.text = "";
-				        TalkButton.SetActive(false);
-				        ButtonText1.text = "";
-				 // nextButton.SetActive(false);
-                // allowSpace = false;
+        else if (primeInt == 3)
+        {
+            ArtBG3.SetActive(false);
+            ArtBG1.SetActive(true);
+            ArtChar1.SetActive(true);
+            Char1Plate.SetActive(false);
+            Char2Plate.SetActive(true);
+            Char3Plate.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = playerName;
+            Char2speech.text = "Go!";
+            Char3name.text = "";
+            Char3speech.text = "";
+            Char4speech_shake.text = "";
+            TalkButton.SetActive(false);
+            nextButton.SetActive(true);
+            // nextButton.SetActive(false);
+            // allowSpace = true;
 
-		}
-    else if (primeInt == 4){
+        }
+        else if (primeInt == 4)
+        {
             DialogueDisplay.SetActive(false);
             ArtBG1.SetActive(false);
             ArtBG2.SetActive(true);
@@ -137,78 +145,86 @@ public void talking(){         // main story function. Players hit next to progr
             Char3name.text = "";
             Char3speech.text = "";
             Char4speech_shake.text = "";
-            TalkButton.SetActive(false);
+            nextButton.SetActive(true);
+            // nextButton.SetActive(false);
+            // allowSpace = true;
+
+        }
+        else if (primeInt == 5)
+        {
+            DialogueDisplay.SetActive(false);
+            ArtBG2.SetActive(true);
+            ArtChar2.SetActive(false);
+            ArtChar22.SetActive(true);
+            Char1Plate.SetActive(false);
+            Char2Plate.SetActive(false);
+            Char3Plate.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "";
+            Char3speech.text = "";
+            Char4speech_shake.text = "";
+            nextButton.SetActive(true);
             ButtonText1.text = "";
-     // nextButton.SetActive(false);
-            // allowSpace = false;
-
-}
-else if (primeInt == 5){
-        DialogueDisplay.SetActive(false);
-        ArtBG2.SetActive(true);
-        ArtChar2.SetActive(false);
-        ArtChar22.SetActive(true);
-        Char1Plate.SetActive(false);
-        Char2Plate.SetActive(false);
-        Char3Plate.SetActive(false);
-        Char1name.text = "";
-        Char1speech.text = "";
-        Char2name.text = "";
-        Char2speech.text = "";
-        Char3name.text = "";
-        Char3speech.text = "";
-        Char4speech_shake.text = "";
-        TalkButton.SetActive(false);
-        ButtonText1.text = "";
- // nextButton.SetActive(false);
-        // allowSpace = false;
-}
-    else if (primeInt == 6){
-      DialogueDisplay.SetActive(true);
-                Char3Plate.SetActive(true);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-                Char3name.text = "Lieutenant Rory";
-                Char3speech.text = "They’re attacking! ";
-				        Char4speech_shake.text = "";
-				        ArtLaser1.SetActive(true);
-				        TalkButton.SetActive(true);
-				        ButtonText1.text = "Fire!!";
-				        nextButton.SetActive(false);
-                allowSpace = false;
-				        ArtChar3.SetActive(true);
-				        ArtChar4.SetActive(true);
+            // nextButton.SetActive(false);
+            // allowSpace = true;
         }
-       else if (primeInt == 7){
-                Char3Plate.SetActive(false);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-                Char3name.text = "";
-                Char3speech.text = "";
-				        Char4speech_shake.text = "Fire! Fire! Fire!";
-				        ArtChar2.SetActive(true);
-				        ArtLaser2.SetActive(true);
-				        TalkButton.SetActive(false);
-				        nextButton.SetActive(false);
+        else if (primeInt == 6)
+        {
+            DialogueDisplay.SetActive(true);
+            Char3Plate.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "Lieutenant Rory";
+            Char3speech.text = "They’re attacking! ";
+            Char4speech_shake.text = "";
+            ArtLaser1.SetActive(true);
+            ArtLaser2.SetActive(false);
+            ArtLaser3.SetActive(false);
+            ArtLaser4.SetActive(false);
+            ArtLaser5.SetActive(false);
+            TalkButton.SetActive(true);
+            ButtonText1.text = "Fire!!";
+            nextButton.SetActive(false);
+            allowSpace = true;
+            ArtChar3.SetActive(true);
+            ArtChar4.SetActive(true);
+        }
+        else if (primeInt == 7)
+        {
+            Char3Plate.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "";
+            Char3speech.text = "";
+            Char4speech_shake.text = "";
+            ArtLaser1.SetActive(true);
+            ArtLaser2.SetActive(true);
+            ArtLaser3.SetActive(true);
+            ArtLaser4.SetActive(true);
+            ArtLaser5.SetActive(true);
+            ArtChar2.SetActive(false);
+            DialogueDisplay.SetActive(false);
+
+            TalkButton.SetActive(false);
+            nextButton.SetActive(true);
 
 
         }
-		else if (primeInt == 8){
-				 Char4speech_shake.text = "Fire! Fire! Fire! Fire! FIRE!";
-				 ArtLaser3.SetActive(true);
-
-        }
-		else if (primeInt == 9){
-                Char1name.text = "";
-              	 TalkButton.SetActive(false);
-				 nextButton.SetActive(false);
-				 NextScene1Button.SetActive(true);
-				 ArtLaser4.SetActive(true);
-				 Char4speech_shake.text = "Fire! Fire! Fire! Fire! FIRE! FIRE!";
+        else if (primeInt == 8)
+        {
+            Char1name.text = "";
+            TalkButton.SetActive(false);
+            nextButton.SetActive(false);
+            NextScene1Button.SetActive(true);
+            ArtLaser4.SetActive(true);
+            Char4speech_shake.text = "";
         }
 
 		if(Char1name.text != "") {
@@ -243,7 +259,7 @@ else if (primeInt == 5){
 	   public void resetButtons(){
         NextScene1Button.SetActive(false);
 		    nextButton.SetActive(false);
-        allowSpace = false;
+        allowSpace = true;
 		    TalkButton.SetActive(false);
 	   }
 
